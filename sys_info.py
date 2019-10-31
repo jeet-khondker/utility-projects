5654752c68407e3c9023360326ff53bcf9e8067c
Jeet Khondker <jeet.usolutionsgroup@gmail.com>
2019-11-01 06:27:26 +0900

    CPU Information Code Added

--- sys_info.py
+++ sys_info.py
@@ -17,6 +17,7 @@ def get_size(bytes, suffix = 'B'):
             return f"{bytes:.2f}{unit}{suffix}" # 1.17GB
         bytes /= factor # bytes = bytes / factor
 
+# System Information
 print("=" * 40, "System Information", "=" * 40)
 username = platform.uname()
 print(f"System: {username.system}")
@@ -25,6 +26,30 @@ print(f"Release: {username.release}")
 print(f"Version: {username.version}")
 print(f"Machine: {username.machine}")
 print(f"Processor: {username.processor}")
+
+# Boot Time
+print("=" * 40, "Boot Time", "=" * 40)
+boot_time_timestamp = psutil.boot_time()
+boot_time = datetime.fromtimestamp(boot_time_timestamp)
+print(f"Boot Time: {boot_time.year}/{boot_time.month}/{boot_time.day} {boot_time.hour}:{boot_time.minute}:{boot_time.second}")
+
+# CPU Information
+print("=" * 40, "CPU Information", "=" * 40)
+
+# Number Of Cores
+print("Physical Cores: ", psutil.cpu_count(logical = False))
+print("Total Cores: ", psutil.cpu_count(logical = True))
+
+# CPU Frequencies
+cpu_frequency = psutil.cpu_freq()
+print(f"Maximum Frequency: {cpu_frequency.max: .2f}MHz")
+print(f"Minimum Frequency: {cpu_frequency.min: .2f}MHz")
+print(f"Current Frequency: {cpu_frequency.current: .2f}MHz")
+
+# CPU Usage
+for core_number, percentage in enumerate(psutil.cpu_percent(percpu = True)):
+    print(f"Core {core_number}: {percentage}%")
+print(f"Total CPU Usage: {psutil.cpu_percent()}%")
 
 
 
